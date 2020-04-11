b0326e67276865356aeaeb8d17d17ea680179ea3
Povilas Karvelis <37621154+frank-pk@users.noreply.github.com>
2020-04-11 13:51:36 +0100

    Add files via upload

--- daboxplot/daboxplot.m
+++ daboxplot/daboxplot.m
@@ -30,19 +30,21 @@ function handles = daboxplot(Y,varargin)
 %   'groups'          A vector containing grouping variables. By default
 %                     assumes a single group for a matrix data input. 
 %
-%   'fill'            0 - non-colored boxplots (contrours only)
-%                     1 - boxplots filled with color
+%   'fill'            0 - non-filled boxplots (contrours only)
+%                     1 - boxplots filled with color (default)
 %
 %   'colors'          The RGB matrix for box colors of different groups
 %                     (each row corresponding to a different group). If
 %                     boxplots are filled, these are the fill colors with 
 %                     the edges being black. If boxplots are not filled,
-%                     these colors are used for edges. 
-%                     Default: default matlab colors. 
+%                     these colors are used for edges. These colors can be 
+%                     also used for scatter data instead (see 'flipcolors')
+%                     Default colors: default matlab colors. 
 %   
-%   'whiskers'        0 - no whiskers
-%                     1 - 2nd & 98th percentile (default)
-%                     2 - 9th & 91th percentile
+%   'whiskers'        Draws whiskers to show min and max data values after 
+%                     disregarding the outliers (see outlier description)
+%                     0 - no whiskers
+%                     1 - draw whiskers (default)                     
 %
 %   'scatter'         0 - no datta scatter (deffault)
 %                     1 - on top of the boxplot 
@@ -51,16 +53,20 @@ function handles = daboxplot(Y,varargin)
 %   'scattercolors'   Colors for the scattered data: {face, edge}
 %                     Default: {'k','w'}
 %
+%   'flipcolors'      Will flip the colors of scatter points and boxplots
+%                     0 - boxplots colored by group (default)
+%                     1 - scatter is colored by group
+%
 %   'scatteralpha'    Transparency of scattered data (between 0 and 1)
 %                     Default: 1 (completely non-transparent)
 %
-%   'jitter'          0 - do not jitter scattered data (default)
-%                     1 - jitter scattered data
+%   'jitter'          0 - do not jitter scattered data 
+%                     1 - jitter scattered data (default)
 % 
 %   'outliers'        Highlights the outliers in the plot. The outliers 
-%                     are determined based on the choice of whiskers.
-%                     0 - do not highlight outliers (default) 
-%                     1 - highlight outliers
+%                     are values below Q1-1.5*IQR and above Q3+1.5*IQR.
+%                     0 - do not highlight outliers  
+%                     1 - highlight outliers (default)
 %
 %   'symbol'          Symbol and color for highlighting outliers.
 %                     Default: 'rx' (red crosses).
@@ -68,8 +74,18 @@ function handles = daboxplot(Y,varargin)
 %   'boxalpha'        Boxplot transparency (between 0 and 1)
 %                     Default: 1 (completely non-transparent)
 %
+%   'boxspacing'      Scales spacing between boxes in the same condition. 
+%                     Note that spacing is also dependent on box width
+%                     Default: 1
+%
+%   'boxwidth'        Scales the width of all boxes
+%                     Default: 1
+%
 %   'conditions'      Xtick labels for conditions (a cell of chars)
 %                     Default: conditions are numbered in the input order.
+%
+%   'legend'          Names of groups (a cell) for creating a legend
+%                     Default: no legend
 %
 % Output Arguments:
 %
@@ -82,6 +98,7 @@ function handles = daboxplot(Y,varargin)
 %       sc - scattered data markers (graphics object)
 %       ot - outlier markers (graphics object)
 %       wh - whiskers (graphics object)
+%       lg - legend (graphics object)
 %
 %
 % For examples have a look at daboxplot_demo.m
@@ -102,18 +119,23 @@ addOptional(p, 'fill', 1);
 addOptional(p, 'colors', get(gca,'colororder'));
 addOptional(p, 'whiskers', 1);
 addOptional(p, 'scatter', 0); 
-addOptional(p, 'scattersize', 20)
+addOptional(p, 'scattersize', 15)
 addOptional(p, 'scattercolors', {'k','w'}); 
+addOptional(p, 'flipcolors', 0);
 addOptional(p, 'scatteralpha', 1); 
-addOptional(p, 'jitter', 0);
-addOptional(p, 'outliers', 0); 
+addOptional(p, 'jitter', 1);
+addOptional(p, 'outliers', 1); 
 addOptional(p, 'symbol', 'rx'); 
 addOptional(p, 'boxalpha', 1);
-addOptional(p, 'conditions', []); 
+addOptional(p, 'boxspacing', 1);
+addOptional(p, 'boxwidth', 1);
+addOptional(p, 'conditions', []);
+addOptional(p, 'legend', []);
+
 
 % parse the input options
 parse(p, varargin{:});
-confs = p.Results;
+confs = p.Results;    
 
 % get group indices and labels if 
 if ~isempty(confs.groups)
@@ -160,10 +182,10 @@ num_locs = numel(cpos);
 gpos=[];
 if num_groups==1
     gpos = cpos;
-    box_width = 1/3;
+    box_width = 1/3*confs.boxwidth;
 else
-    box_width = (2/3)/(num_groups+1);   % calculate box width 
-    loc_sp = box_width/4;   % gap of 1/4 box width between boxplots
+    box_width = (2/3)/(num_groups+1)*confs.boxwidth;  % calculate box width 
+    loc_sp = box_width/3*confs.boxspacing; % local spacing between boxplots
     
     % set group positions for each group
     for g = 1:num_groups
@@ -174,13 +196,13 @@ end
 handles.gpos = gpos;
 handles.cpos = cpos; 
 
-
 % loop over groups
 for g = 1:num_groups 
     
     % get percentiles
-    pt = prctile(Y(Gi==g,:),[2 9 25 50 75 91 98]);   
-    
+    pt = prctile(Y(Gi==g,:),[2 9 25 50 75 91 98]); 
+    IQR = (pt(5,:)-pt(3,:));
+        
     % create coordinates for drawing boxes
     y25 = reshape([pt(3,:); pt(3,:)], 1, []);
     y75 = reshape([pt(5,:); pt(5,:)], 1, []);
@@ -188,30 +210,39 @@ for g = 1:num_groups
     x1 = [gpos(g,:) - box_width/2; gpos(g,:) - box_width/2];
     x2 = [gpos(g,:) + box_width/2; gpos(g,:) + box_width/2];
 
-    box_ycords = [y75; y25];        
-    box_xcords = reshape([x1; x2],2,[]); 
-    box_medcords = reshape([pt(4,:); pt(4,:)], 1, []);
+    box_ycor = [y75; y25];        
+    box_xcor = reshape([x1; x2],2,[]); 
+    box_medcor = reshape([pt(4,:); pt(4,:)], 1, []);
     
     % create coordinates for drawing whiskers with cross-hatches and ends    
-    hat_xcords = [gpos(g,:) - box_width/4; gpos(g,:) + box_width/4];    
-    whi_xcords = [gpos(g,:); gpos(g,:)];
+    hat_xcor = [gpos(g,:) - box_width/4; gpos(g,:) + box_width/4];    
+    whi_xcor = [gpos(g,:); gpos(g,:)];        
     
     % draw one box at a time
     for k = 1:num_locs
+        
+        data_vals = Y(Gi==g,k); % data for a single box
+        
+        % determine outliers and whisker length 
+        ol = data_vals<(pt(3,k)-IQR(k)); % indices of lower outliers
+        ou = data_vals>(pt(5,k)+IQR(k)); % indices of upper outliers    
 
+        whi_ycor(:,1,k) = [min(data_vals(~ol)), pt(3,k)]; % lower whisker        
+        whi_ycor(:,2,k) = [max(data_vals(~ou)), pt(5,k)]; % upper whisker        
+        
         % jitter or not
         if confs.jitter==1
             xdata =  gpos(g,k).*ones(numel(Y(Gi==g,k)),1) + ...
-                (box_width/2).*(0.5 - rand(numel(Y(Gi==g,k)),1));
+                (box_width/3).*(0.5 - rand(numel(Y(Gi==g,k)),1));
         elseif confs.jitter==0
             xdata = gpos(g,k).*ones(numel(Y(Gi==g,k)),1);
         end
-        data_vals = Y(Gi==g,k);
+        
 
         % index values for each box
         win_k = (1:2)+2*(k-1);
-        Xx = box_xcords(1:2,win_k); 
-        Yy = box_ycords(1:2,win_k); 
+        Xx = box_xcor(1:2,win_k); 
+        Yy = box_ycor(1:2,win_k); 
 
         % filled or not filled boxes
         if confs.fill==0            
@@ -222,53 +253,39 @@ for g = 1:num_groups
             hold on;        
             
             % median
-            m(k,g) = line(Xx(1,:), box_medcords(win_k),...
+            m(k,g) = line(Xx(1,:), box_medcor(win_k),...
                 'color',confs.colors(g,:), 'LineWidth', 2);
             
         elseif confs.fill==1
             % box filled with color 
             bx(k,g) = fill([Xx(:,1)' Xx(1,:) Xx(:,2)' Xx(2,[2,1])],...
                  [Yy(:,1)' Yy(1,:) Yy(:,2)' Yy(2,:)],confs.colors(g,:));            
-            alpha(confs.boxalpha); 
+            set(bx(k,g),'FaceAlpha',confs.boxalpha); 
             hold on;
 
             % draw the median
-            m(k,g) = line(Xx(1,:), box_medcords(win_k),...
+            m(k,g) = line(Xx(1,:), box_medcor(win_k),...
                 'color','k', 'LineWidth', 2);
-        end
-        
-        % if no whiskers, use boxplot edges to determine outliers
-        whi_ends = pt([3,5],k); 
-        
-        if confs.whiskers==1
-
-            wh(k,g,:) = plot(whi_xcords(:,k),pt([1,3],k),'k-',... % lower whisker  
-                hat_xcords(:,k),[pt(1,k) pt(1,k)],'k-',... % lower end (2%)
-                whi_xcords(:,k),pt([5,7],k),'k-',... % upper whisker
-                hat_xcords(:,k),[pt(7,k) pt(7,k)],'k-',... % upper end (98%)
-                'LineWidth',1); 
-            
-            whi_ends = pt([1,7],k);
-            
-        elseif confs.whiskers==2
-            
-            wh(k,g,:) = plot(whi_xcords(:,k),pt([2,3],k),'k-',... % lower whisker
-                hat_xcords(:,k),[pt(2,k) pt(2,k)],'k-',... % lower end (9%)
-                whi_xcords(:,k),pt([5,6],k),'k-',... % upper whisker
-                hat_xcords(:,k),[pt(6,k) pt(6,k)],'k-',... % upper end (91%)
-                'LineWidth',1);  
-            
-            whi_ends = pt([2,6],k);           
         end
         
         ox = data_vals>max(data_vals); % default - no outliers
         
         % draw outliers
         if confs.outliers==1            
-            ox = data_vals<whi_ends(1) |  data_vals>whi_ends(2);
+            ox = data_vals<whi_ycor(1,1,k) |  data_vals>whi_ycor(1,2,k);
             ot(k,g) = scatter(xdata(ox),data_vals(ox),confs.scattersize,...
                 confs.symbol);            
-        end       
+        end    
+        
+        
+        if confs.whiskers==1
+            % draw whiskers
+            wh(k,g,:) = plot(whi_xcor(:,k),whi_ycor(:,1,k),'k-',... 
+                hat_xcor(:,k),[whi_ycor(1,1,k) whi_ycor(1,1,k)],'k-',... 
+                whi_xcor(:,k),whi_ycor(:,2,k),'k-',... 
+                hat_xcor(:,k),[whi_ycor(1,2,k) whi_ycor(1,2,k)],'k-',... 
+                'LineWidth',1);                              
+        end 
 
         % scatter on top of the boxplots
         if confs.scatter==1 || confs.scatter==2            
@@ -286,6 +303,35 @@ for g = 1:num_groups
         uistack(sc(:,g),'bottom')
     end
     
+end
+
+% flip scatter and box colors and make a legend
+if confs.flipcolors==1    
+    
+    box_class = class(bx); % box filled or no
+    
+    if strcmp(box_class,'matlab.graphics.primitive.Patch')
+        set(bx,'FaceColor',confs.scattercolors{1});
+        set(m,'Color',confs.scattercolors{2});
+    else
+        set(bx,'Color',confs.scattercolors{1});
+        set(m,'Color',confs.scattercolors{1});
+    end
+
+    for g = 1:num_groups
+       set(sc(:,g),'MarkerFaceColor',confs.colors(g,:))
+    end
+    
+    % add a legend based on scatter colors
+    if ~isempty(confs.legend)
+        handles.lg = legend(sc(1,:),confs.legend);
+    end
+else
+    
+    % add a legend based on box colors
+    if ~isempty(confs.legend)
+        handles.lg = legend(bx(1,:),confs.legend);
+    end
 end
 
 handles.bx = bx; 
--- daboxplot/daboxplot_demo.m
+++ daboxplot/daboxplot_demo.m
@@ -14,9 +14,9 @@ data1{3} = randn([60,4]); % God
 data1{4} = randn([60,4]); % Potato
 
 % data in a matrix (+ grouping indices)
-data2 = [randn([20,4]); randn([20,4]);...
-         randn([20,4]); randn([20,4])];
-group_inx = [ones(1,20), 2.*ones(1,20) 3.*ones(1,20) 4.*ones(1,20)];
+data2 = [randn([30,4]); randn([30,4]);...
+         randn([30,4]); randn([30,4])];
+group_inx = [ones(1,30), 2.*ones(1,30) 3.*ones(1,30) 4.*ones(1,30)];
 
 group_names = {'Humans', 'Dogs' , 'God', 'Potato'};
 condition_names = {'Water', 'Land', 'Moon', 'Hyperspace'};
@@ -25,65 +25,60 @@ condition_names = {'Water', 'Land', 'Moon', 'Hyperspace'};
 c =  [0.45, 0.80, 0.69;...
       0.98, 0.40, 0.35;...
       0.55, 0.60, 0.79;...
-      0.90, 0.70, 0.30];
+      0.90, 0.70, 0.30];  
   
 figure('Name', 'daboxplot_demo','WindowStyle','docked');
 pause(0.5); % prevents a glitch in xlim; don't ask me why
 
-% the default style
+
+% filled boxplots with no outliers
 subplot(3,2,1)
-handles = daboxplot(data1);
+h = daboxplot(data1,'outliers',0,'legend',group_names);
 xticklabels(condition_names);
 ylabel('Performance');
-xl = xlim; xlim([xl(1), xl(2)+1]); % make space for the legend
-legend([handles.bx(1,:)],group_names)
-set(gca,'box','off'); ylim([-4 4])
+xl = xlim; xlim([xl(1), xl(2)+1]);    % make more space for the legend
+set(h.lg,'FontSize',9);               % make the legend bigger
+ylim([-4 4])
 
-% non-filled boxplots
+
+% non-filled boxplots, different color scheme and cutomized medians
 subplot(3,2,2)
-handles = daboxplot(data2,'groups',group_inx,...
-    'conditions', condition_names,'fill',0);
+h = daboxplot(data2,'groups',group_inx,'colors',c,...
+    'conditions', condition_names,'fill',0,'legend',group_names);
 ylabel('Performance');
-xl = xlim; xlim([xl(1), xl(2)+1]); % make space for the legend
-legend([handles.bx(1,:)],group_names)
-set(handles.wh,'Color',[0.2 0.6 0.2],'LineWidth',1.5); % customize whiskers
+xl = xlim; xlim([xl(1), xl(2)+1]);    % make more space for the legend
+set(h.m,'Color','k','LineWidth',1.5); % customize median lines
+set(h.lg,'FontSize',9);               % make the legend bigger
+
 
 % transparent boxplots with no whiskers and jittered datapoints underneath
 subplot(3,2,3)
-handles = daboxplot(data1,'scatter',2,'whiskers',0,...
-    'jitter',1,'boxalpha',0.7,'conditions', condition_names); 
+h = daboxplot(data1,'scatter',2,'whiskers',0,'boxalpha',0.7,...
+    'conditions', condition_names); 
 ylabel('Performance');
-xl = xlim; xlim([xl(1), xl(2)+1]); % make space for the legend
-legend([handles.bx(1,:)],group_names)
+xl = xlim; xlim([xl(1), xl(2)+1]);            % make space for the legend
+legend([h.bx(1,:)],group_names,'FontSize',9); % add the legend manually
 
-% different color scheme, shorter whiskers and outliers only
+% different color scheme, a color flip, different outlier symbol
 subplot(3,2,4)
-handles = daboxplot(data2,'groups',group_inx,...
-    'conditions', condition_names,'colors',c,'fill',0,'whiskers',2,...
-    'jitter',1,'outliers',1,'scattersize',30); 
+h = daboxplot(data2,'groups',group_inx,'conditions', condition_names,...
+    'colors',c,'fill',0,'whiskers',0,'scatter',2,'symbol','k*',...
+    'outliers',1,'scattersize',16,'flipcolors',1,'boxspacing',1.2,...
+    'legend',group_names); 
 ylabel('Performance');
-xl = xlim; xlim([xl(1), xl(2)+1]); % make space for the legend
-legend([handles.bx(1,:)],group_names)
-set(handles.m,'Color','k','LineWidth',1.5); % customize median lines
+xl = xlim; xlim([xl(1), xl(2)+1]); % make more space for the legend
+set(h.lg,'FontSize',9);            % make the legend bigger
 
 
-% different color scheme, no whiskers, no jitter, data scattered underneath
-subplot(3,2,5)
-handles = daboxplot(data2,'colors',c,'groups',group_inx,...
-    'scatter',2,'whiskers',0,'conditions', condition_names); 
+% different color scheme, data scattered on top
+subplot(3,2,5:6)
+h = daboxplot(data2,'groups',group_inx,...
+    'conditions', condition_names,'colors',c,'whiskers',0,'fill',1,...
+    'scatter',1,'scattersize',15,'outliers',1,'scatteralpha',0.5,...
+    'boxspacing',0.8,'legend',group_names); 
 ylabel('Performance');
-xl = xlim; xlim([xl(1), xl(2)+1]); % make space for the legend
-legend([handles.bx(1,:)],group_names)
-
-% different color scheme, data scattered underneath as small dots
-subplot(3,2,6)
-handles = daboxplot(data1,'conditions', condition_names,'colors',c,...
-    'whiskers',1,'fill',1,'scatter',2,'jitter',1,'scattersize',15,...
-    'outliers',1,'boxalpha',0.7); 
-ylabel('Performance');
-xl = xlim; xlim([xl(1), xl(2)+1]); % make space for the legend
-legend([handles.bx(1,:)],group_names)
-set(handles.wh,'Color','none'); % customize whiskers
+xl = xlim; xlim([xl(1), xl(2)+0.5]); % make space for the legend
+set(h.lg,'FontSize',9);              % make the legend bigger
 
 
 % TIP: to make the plots vertical use camroll(-90)
